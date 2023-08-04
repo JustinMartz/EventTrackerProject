7d70adcc5210a6dea25d9146123df19b362dee61
Justin Martz <justinmartz@protonmail.com>
2023-08-04 17:59:33 -0600

    Create Spring Boot REST project, link JPA project dependency

---
+++ GuitarTech/src/main/java/com/skilldistillery/guitartech/GuitarTechApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.guitartech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarTechApplication.class, args);
+	}
+
+}
---
+++ GuitarTech/src/main/java/com/skilldistillery/guitartech/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.guitartech;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GuitarTechApplication.class);
+	}
+
+}
