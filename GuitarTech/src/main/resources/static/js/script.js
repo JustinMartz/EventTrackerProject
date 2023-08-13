3b39ed6a5a12385bfd24a009c2622f1ba4839525
Justin Martz <justinmartz@protonmail.com>
2023-08-13 16:43:44 -0600

    Good looking update guitar form

--- GuitarTech/src/main/resources/static/js/script.js
+++ GuitarTech/src/main/resources/static/js/script.js
@@ -1,4 +1,5 @@
 console.log('script.js LOADED');
+var guitars = [];
 
 window.addEventListener('load', function(e) {
 	console.log('page LOADED');
@@ -9,6 +10,7 @@ window.addEventListener('load', function(e) {
 function init() {
 	console.log('in init()');
 	loadGuitars();
+	
 }
 
 function loadGuitars() {
@@ -17,7 +19,7 @@ function loadGuitars() {
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4 && xhr.status === 200) {
 			// do stuff
-			let guitars = JSON.parse(xhr.responseText);
+			guitars = JSON.parse(xhr.responseText);
 			console.log(guitars);
 			displayGuitars(guitars);
 		} else {
@@ -35,6 +37,7 @@ function displayGuitars(guitars) {
 			// make guitar-container div
 			let gcon = document.createElement('div');
 			gcon.className = 'guitar-container';
+			gcon.id = 'guitar_' + g.id;
 			
 			// make image div
 			let gimgdiv = document.createElement('div');
@@ -49,8 +52,7 @@ function displayGuitars(guitars) {
 			let gh1 = document.createElement('h1');
 			gh1.textContent = g.make + ' ' + g.model;
 			gtxt.appendChild(gh1);
-			
-			
+					
 			// make li for year
 			let yearH2 = document.createElement('h2');
 			yearH2.textContent = g.year;
@@ -84,20 +86,23 @@ function displayGuitars(guitars) {
 			setupH2.textContent = 'Last setup: ' + g.setup;
 			gtxt.appendChild(setupH2);
 			
-			
-			
 			// make icons div
 			let gIcons = document.createElement('div');
 			gIcons.className = 'icons';
 			
 			let gEdit = document.createElement('img');
 			gEdit.src = 'images/edit.png';
+			gEdit.id = 'editGuitarIcon';
+			gEdit.title = 'Edit this guitar';
+			gEdit.addEventListener('click', editGuitarCB);
 			gIcons.appendChild(gEdit);
+			
 			let gRem = document.createElement('img');
 			gRem.src = 'images/delete.png';
+			gRem.id = 'deleteGuitarIcon';
+			gRem.title = 'Delete this guitar';
+			gRem.addEventListener('click', deleteGuitarCB);
 			gIcons.appendChild(gRem);
-			
-			
 			
 			gimgdiv.appendChild(gimg);  // attach img element to image div
 			gcon.appendChild(gimgdiv);  // attach image div to guitar-container
@@ -107,5 +112,313 @@ function displayGuitars(guitars) {
 		}
 		
 	}
+	
+}
+
+let editGuitarCB = function(e) {
+		e.preventDefault();
+		let guitarId = e.target.parentNode.parentNode.id;
+		console.log(guitarId);
+		let GIDArray = guitarId.split("_");
+		console.log(GIDArray[1]);
+		guitarId = GIDArray[1];
+		
+		
+		let currentGuitar = guitars[guitarId - 1];
+		console.log(currentGuitar);
+		
+		let gCon = document.getElementById('guitar_' + guitarId); // find current guitar-container to edit
+		console.log('first child element: ' + gCon.firstElementChild.nextElementSibling);
+		gCon.firstElementChild.nextElementSibling.remove();	// remove it
+		// gCon = document.createElement('div');  // create a new one
+		// gCon.className = 'guitar-container';
+		
+		// change icons
+		gCon.lastElementChild.remove();
+		let editIconsDiv = document.createElement('div');
+		editIconsDiv.className = 'icons';
+		let saveIcon = document.createElement('img');
+			saveIcon.src = 'images/save.png';
+			saveIcon.id = 'saveEditedGuitar';
+			saveIcon.title = 'Update guitar info';
+			// FIXME saveIcon.addEventListener('click', updateGuitarCB);
+			editIconsDiv.appendChild(saveIcon);
+			
+		let cancelIcon = document.createElement('img');
+			cancelIcon.src = 'images/cancel.png';
+			cancelIcon.id = 'cancelEditingGuitar';
+			cancelIcon.title = 'Cancel';
+			// FIXME cancelIcon.addEventListener('click', cancelGuitarEditCB);
+			editIconsDiv.appendChild(cancelIcon);
+		
+		gCon.appendChild(editIconsDiv);
+		
+		
+		let gText = document.createElement('div');
+		gText.className = 'text';
+		
+		// construct the form
+		let gForm = document.createElement('form');
+		
+		let makeText = document.createElement('h2');
+		makeText.textContent = 'Make: ';
+		makeText.style.margin = 0;
+		makeText.style.display = 'inline';
+		
+		let makeInput = document.createElement('input');
+		makeInput.type = 'text';
+		makeInput.value = currentGuitar.make;
+		
+		gForm.appendChild(makeText);
+		gForm.appendChild(makeInput); // attach make input to the form
+		
+		let makeBr = document.createElement('br');
+		gForm.appendChild(makeBr);      // line break before next input
+		
+		let modelText = document.createElement('h2');
+		modelText.textContent = 'Model: ';
+		modelText.style.margin = 0;
+		modelText.style.display = 'inline';
+		
+		let modelInput = document.createElement('input');
+		modelInput.type = 'text';
+		modelInput.value = currentGuitar.model;
+		
+		gForm.appendChild(modelText);
+		gForm.appendChild(modelInput); // attach model input to the form
+		
+		let modelBr = document.createElement('br');
+		gForm.appendChild(modelBr);      // line break before next input
+		
+		let yearText = document.createElement('h2');
+		yearText.textContent = 'Year: ';
+		yearText.style.margin = 0;
+		yearText.style.display = 'inline';
+		gForm.appendChild(yearText);
+		
+		let yearInput = document.createElement('select');
+		yearInput.id = 'year-select';
+		yearInput.name = 'year';
+		gForm.appendChild(yearInput);
+		for (let i = 1900; i < 2024; i++) {
+			let yearOption = document.createElement('option');
+			yearOption.textContent = i;
+			yearOption.value = i;
+			if (i === currentGuitar.year) {
+				console.log('i matches year: ' + i);
+				yearOption.selected = true;
+			}
+			yearInput.appendChild(yearOption);
+		}
+		
+		let yearBr = document.createElement('br');
+		gForm.appendChild(yearBr);      // line break before next input
+		
+		let colorText = document.createElement('h2');
+		colorText.textContent = 'Color: ';
+		colorText.style.margin = 0;
+		colorText.style.display = 'inline';
+		
+		let colorInput = document.createElement('input');
+		colorInput.type = 'text';
+		colorInput.value = currentGuitar.color;
+		
+		gForm.appendChild(colorText);
+		gForm.appendChild(colorInput);
+		let colorBr = document.createElement('br');
+		gForm.appendChild(colorBr);
+		
+		let scaleText = document.createElement('h2');
+		scaleText.textContent = 'Scale length: ';
+		scaleText.style.margin = 0;
+		scaleText.style.display = 'inline';
+		
+		let scaleInput = document.createElement('input');
+		scaleInput.type = 'text';
+		scaleInput.value = currentGuitar.scaleLength;
+		
+		gForm.appendChild(scaleText);
+		gForm.appendChild(scaleInput);
+		let scaleBr = document.createElement('br');
+		gForm.appendChild(scaleBr);
+		
+		
+		gText.appendChild(gForm); // attach form text area;
+		gCon.appendChild(gText);  // attach text area to guitar-container
+		// document.getElementById('guitars-container').appendChild(gCon); // attach guitar-container to guitars-container
+		
+		let fretsText = document.createElement('h2');
+		fretsText.textContent = 'Number of frets: ';
+		fretsText.style.margin = 0;
+		fretsText.style.display = 'inline';
+		gForm.appendChild(fretsText);
+		
+		let fretsInput = document.createElement('select');
+		fretsInput.id = 'frets-select';
+		fretsInput.name = 'frets';
+		gForm.appendChild(fretsInput);
+		for (let i = 19; i < 28; i++) {
+			let fretsOption = document.createElement('option');
+			fretsOption.textContent = i;
+			fretsOption.value = i;
+			if (i === currentGuitar.numberOfFrets) {
+				console.log('i matches number of frets: ' + i);
+				fretsOption.selected = true;
+			}
+			fretsInput.appendChild(fretsOption);
+		}
+		
+		gForm.appendChild(fretsText);
+		gForm.appendChild(fretsInput);
+		let fretsBr = document.createElement('br');
+		gForm.appendChild(fretsBr);
+		
+		let caseText = document.createElement('h2');
+		caseText.textContent = 'Has a case: ';
+		caseText.style.margin = 0;
+		caseText.style.display = 'inline';
+		gForm.appendChild(caseText);
+		
+		let caseInput = document.createElement('fieldset');
+		gForm.appendChild(caseInput);
+		
+		let caseYesInput = document.createElement('input');
+		caseYesInput.type = 'radio';
+		caseYesInput.id = 'case-yes';
+		caseYesInput.name = 'hasCase';
+		caseYesInput.value = true;
+		if (currentGuitar.hasCase === true) {
+			caseYesInput.checked = true;
+		}
+		
+		let caseYesInputLabel = document.createElement('label');
+		caseYesInputLabel.for = caseYesInput.id;
+		caseYesInputLabel.textContent = 'Yes';
+		console.log('label.for = ' + caseYesInputLabel.for);
+		
+		caseInput.appendChild(caseYesInput);
+		caseInput.appendChild(caseYesInputLabel);
+		
+		let caseNoInput = document.createElement('input');
+		caseNoInput.type = 'radio';
+		caseNoInput.id = 'case-no';
+		caseNoInput.name = 'hasCase';
+		caseNoInput.value = false;
+		if (currentGuitar.hasCase === false) {
+			caseYesInput.checked = true;
+		}
+		
+		let caseNoInputLabel = document.createElement('label');
+		caseNoInputLabel.for = caseNoInput.id;
+		caseNoInputLabel.textContent = 'No';
+		console.log('label.for = ' + caseNoInputLabel.for);
+		
+		caseInput.appendChild(caseNoInput);
+		caseInput.appendChild(caseNoInputLabel);
+		let caseBr = document.createElement('br');
+		gForm.appendChild(caseBr);
+		
+		let bridgeText = document.createElement('h2');
+		bridgeText.textContent = 'Bridge type: ';
+		bridgeText.style.margin = 0;
+		bridgeText.style.display = 'inline';
+		
+		let bridgeInput = document.createElement('select');
+		bridgeInput.id = 'bridge-select';
+		bridgeInput.name = 'bridge';
+		gForm.appendChild(bridgeInput);
+		
+			let bridgeOptionTOM = document.createElement('option');
+			bridgeOptionTOM.textContent = 'Tune-O-Matic';
+			bridgeOptionTOM.value = 'Tune-O-Matic';
+			if ('Tune-O-Matic' === currentGuitar.bridge) {
+				bridgeOptionTOM.selected = true;
+			}
+			bridgeInput.appendChild(bridgeOptionTOM);
+			
+			let bridgeOptionFloyd = document.createElement('option');
+			bridgeOptionFloyd.textContent = 'Floyd Rose';
+			bridgeOptionFloyd.value = 'Floyd Rose';
+			if ('Floyd Rose' === currentGuitar.bridge) {
+				bridgeOptionFloyd.selected = true;
+			}
+			bridgeInput.appendChild(bridgeOptionFloyd);
+			
+			let bridgeOption3 = document.createElement('option');
+			bridgeOption3.textContent = '3-Saddle';
+			bridgeOption3.value = '3-Saddle';
+			if ('3-Saddle' === currentGuitar.bridge) {
+				bridgeOption3.selected = true;
+			}
+			bridgeInput.appendChild(bridgeOption3);
+		
+		gForm.appendChild(bridgeText);
+		gForm.appendChild(bridgeInput);
+		let bridgeBr = document.createElement('br');
+		gForm.appendChild(bridgeBr);
+		
+		let tuningText = document.createElement('h2');
+		tuningText.textContent = 'Current tuning: ';
+		tuningText.style.margin = 0;
+		tuningText.style.display = 'inline';
+		gForm.appendChild(tuningText);
+		
+		let tuningInput = document.createElement('select');
+		tuningInput.id = 'tuning-select';
+		tuningInput.name = 'tuning';
+		gForm.appendChild(tuningInput);
+		
+			let tuningOptionE = document.createElement('option');
+			tuningOptionE.textContent = 'E Standard';
+			tuningOptionE.value = 'E Standard';
+			if ('E Standard' === currentGuitar.tuning.name) {
+				tuningOptionE.selected = true;
+			}
+			tuningInput.appendChild(tuningOptionE);
+			
+			let tuningOptionEb = document.createElement('option');
+			tuningOptionEb.textContent = 'Eb Standard';
+			tuningOptionEb.value = 'Eb Standard';
+			if ('Eb Standard' === currentGuitar.tuning.name) {
+				tuningOptionEb.selected = true;
+			}
+			tuningInput.appendChild(tuningOptionEb);
+			
+			let tuningOptionD = document.createElement('option');
+			tuningOptionD.textContent = 'D Standard';
+			tuningOptionD.value = 'D Standard';
+			if ('D Standard' === currentGuitar.tuning.name) {
+				tuningOptionD.selected = true;
+			}
+			tuningInput.appendChild(tuningOptionD);
+			
+			let tuningOptionCsharp = document.createElement('option');
+			tuningOptionCsharp.textContent = 'C# Standard';
+			tuningOptionCsharp.value = 'C# Standard';
+			if ('C# Standard' === currentGuitar.tuning.name) {
+				tuningOptionCsharp.selected = true;
+			}
+			tuningInput.appendChild(tuningOptionCsharp);
+			
+			
+		
+
+	/*
+	document.querySelectorAll('.guitar-container').forEach(function(a){
+		a.remove()
+	})
+	*/
+	console.log('in editGuitarCB()');
+}
+
+let deleteGuitarCB = function(e) {
+		e.preventDefault();
+
+	/*
+	document.querySelectorAll('.guitar-container').forEach(function(a){
+		a.remove()
+	})
+	*/
+	console.log('in deleteGuitarCB()');
 }
 
